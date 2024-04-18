a756a7c2da3094f75e55abb64bb6aacc4ab9aae8
rithyas <46305908+rithyas@users.noreply.github.com>
2024-04-18 17:12:01 -0400

    Add files via upload
    Rithya

--- visuals/bar_graph_genre/BarGraphGenre.R
+++ visuals/bar_graph_genre/BarGraphGenre.R
@@ -2,6 +2,7 @@
 library(gganimate)
 library(tidyverse)
 library(ggplot2)
+library(dplyr)
 
 data <-
   read_csv("/Users/rithyasri/Downloads/finalMatched.csv")
@@ -12,45 +13,54 @@ bar <- data %>%
 
 
 # run this for  breakdown
-bar <- bar %>% 
+bar_decade <- bar %>% 
   group_by(genre,decade) %>% 
   summarise(
   sentiment = mean(`Compound Score`)
-  )
-  
-bar <- decade %>%
-  ifelse(. <=10, . + 19, . + 00)
+  ) %>% 
+  mutate(clean_decades = case_when(
+    decade == "10s"~"2010s",
+    decade == "00s"~"2000s",
+    decade == "90s"~"1990s",
+    decade == "80s"~"1980s",
+    decade == "70s"~"1970s",
+    decade == "60s"~"1960s"
+  ))
   
 
-bar %>% 
+
+bar_decade %>% 
   ggplot(aes(x = genre, y = sentiment)) +
-  geom_bar(stat = "identity",   fill = "#cc489c") +
-  labs(title = "Breakdown by Decade",
+  geom_bar(stat = "identity",   fill = "#936eb1") +
+  labs(title = "Sentiment Across The Years",
        x = "Genre",
-       y = "Total Sentiment", subtitle = "Decade: {closest_state}") + 
+       y = "Total Sentiment", subtitle = paste("Semtiment Ranges From Optimsitic(1) to Pessimsitic(-1)",
+            "Decade: {closest_state}", sep = "\n"),
+       caption="By: Rithya Sriram \n Sources: Billboard Top 100, Genius API") + 
   theme_minimal() +
   theme(plot.title = element_text(hjust=0.5),
         plot.subtitle = element_text(hjust=0.5)) +
-  transition_states(decade, transition_length = 1, state_length = 1)
+  transition_states(clean_decades, transition_length = 1, state_length = 1)
 
 
 
 # run this for normal
-bar <- bar %>% 
+bar_normal <- bar %>% 
  group_by(genre) %>% 
   summarise(
    sentiment = mean(`Compound Score`)
  )  %>% 
  arrange(-sentiment)
 
-bar %>% 
+bar_normal %>% 
   ggplot(aes(x = reorder(genre,sentiment), y = sentiment)) +
- labs(title="Sentiment in Comparison to Genre", 
-      subtitle="", 
+ labs(title="Sentiment Across Genre", 
+      subtitle="Semtiment Ranges From Optimsitic(1) to Pessimsitic(-1)", 
       caption="By: Rithya Sriram \n Sources: Billboard Top 100, Genius API") +
 geom_bar(stat = "identity", fill = "#cc489c") +
   theme_minimal()+
-theme(plot.title = element_text(hjust=0.5)) +
+theme(plot.title = element_text(hjust=0.5),
+      plot.subtitle = element_text(hjust=0.5)) +
 ylab("Sentiment") + 
 xlab("Genre")
 
