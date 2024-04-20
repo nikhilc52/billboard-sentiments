7d88bcc1e89fa094b923239f3df22f0acdbdc2cd
Nikhil Chinchalkar <nsc49@cornell.edu>
2024-04-20 05:26:30 -0400

    Update slider.js

--- slider.js
+++ slider.js
@@ -4,3 +4,11 @@ function updateImage() {
     var imageName = directory + "/" + frameID + ".png";
     document.getElementById("imageToChange").src = imageName;
 }
+
+//separate function for instances where mutliple sliders are needed on 1 page (since elements are not unique)
+function updateImage2() {
+    var frameID = document.getElementById("yearID2").value;
+    var directory = document.getElementById("imageToChange2").src.substring(0,document.getElementById("imageToChange2").src.lastIndexOf("/"));
+    var imageName = directory + "/" + frameID + ".png";
+    document.getElementById("imageToChange2").src = imageName;
+}
