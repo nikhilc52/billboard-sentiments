38b4afcf69e82a1a0d9f1fa2e84535e9227be0c2
Tony Oh <do256@cornell.edu>
2024-04-22 03:59:59 -0400

    Slider slight change only on image1

--- slider.js
+++ slider.js
@@ -1,6 +1,6 @@
 function updateImage() {
     var frameID = document.getElementById("yearID").value;
-    var directory = document.getElementById("imageToChange").src.substring(0,document.getElementById("imageToChange").src.lastIndexOf("/"));
+    var directory = document.getElementById("imageToChange").src.substring(0, document.getElementById("imageToChange").src.lastIndexOf("/"));
     var imageName = directory + "/" + frameID + ".png";
     document.getElementById("imageToChange").src = imageName;
 }
@@ -8,7 +8,7 @@ function updateImage() {
 //separate function for instances where mutliple sliders are needed on 1 page (since elements are not unique)
 function updateImage2() {
     var frameID = document.getElementById("yearID2").value;
-    var directory = document.getElementById("imageToChange2").src.substring(0,document.getElementById("imageToChange2").src.lastIndexOf("/"));
+    var directory = document.getElementById("imageToChange2").src.substring(0, document.getElementById("imageToChange2").src.lastIndexOf("/"));
     var imageName = directory + "/" + frameID + ".png";
     document.getElementById("imageToChange2").src = imageName;
 }
@@ -16,7 +16,13 @@ function updateImage2() {
 //separate function for instances where mutliple sliders are needed on 1 page (since elements are not unique)
 function updateImage3() {
     var frameID = document.getElementById("yearID3").value;
-    var directory = document.getElementById("imageToChange3").src.substring(0,document.getElementById("imageToChange3").src.lastIndexOf("/"));
+    var directory = document.getElementById("imageToChange3").src.substring(0, document.getElementById("imageToChange3").src.lastIndexOf("/"));
     var imageName = directory + "/" + frameID + ".png";
     document.getElementById("imageToChange3").src = imageName;
+}
+
+function updateFrame() {
+    var frameID = document.getElementById("yearID").value;
+    var imageName = "writeups/sliders/sentiment_line" + "/" + frameID + ".png";
+    document.getElementById("imageToChange").src = imageName;
 }
